a5d9f834d30794006631afd3e0d820f3fdb14725
Richard Schneider <richard@schneiderbox.net>
2014-09-05 21:01:19 -0500

    add event support to actions

--- js/siopa-engine.js
+++ js/siopa-engine.js
@@ -1,10 +1,24 @@
 'use strict';
 
 function Siopa(startID){
+	var siopa = this;
+
 	this.sceneBox = document.createElement('div');
 	this.sceneBox.id = 'sceneBox';
 	document.body.appendChild(this.sceneBox);
 	
+	document.querySelector('body').addEventListener('click', function(event){
+		var element = event.target;
+		if(event.target.tagName.toLowerCase() == 'action'){
+			var to = element.getAttribute('to');
+			if(to){
+				siopa.activateScene(to);
+			}
+			var text = element.getElementsByTagName('actionText')[0];
+			siopa.eventBox.innerHTML = text.innerHTML;
+		}
+	});
+
 	this.activateScene(startID);
 }
 Siopa.prototype.activateScene = function(sceneID){
@@ -12,12 +26,8 @@ Siopa.prototype.activateScene = function(sceneID){
 	var scene = document.getElementById(sceneID);
 	this.sceneBox.innerHTML = scene.innerHTML;
 
-	var actions = this.sceneBox.getElementsByTagName('action');
-	for(var i = 0; i < actions.length; i++){
-		actions[i].onclick = function(){
-			siopa.activateScene(this.getAttribute('to'));
-		}
-	}
-
+	this.eventBox = document.createElement('div')
+	this.eventBox.id = 'eventBox';
+	this.sceneBox.appendChild(this.eventBox);
 };
 
